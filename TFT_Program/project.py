59e296ead8578052f6b2ff734f733d21d30ccc77
IanBechard <43045250+IanBechard@users.noreply.github.com>
2019-12-16 11:22:56 -0500

    Newest Code

--- TFT_Program/project.py
+++ TFT_Program/project.py
@@ -11,6 +11,9 @@ class Champion:
     def __str__(self):
         return "Champion: {0:8} Cost: {1:1} \tTraits: {2:20}".format(self.name, self.cost, self.traits)
 
+    def printName(self):
+        return self.name
+
 class Trait:
     def __init__(self, name, description, sets):
         self.name = name
@@ -24,9 +27,9 @@ class Item:
 def importChamps():
     with open('TFT_Program/champions.json','r') as f:
         jsonChamps = json.load(f)
-    champsList = []
+    champsList = {}
     for champ in jsonChamps:
-        champsList.append(Champion(champ["champion"], champ["cost"], champ["traits"], champ["image"]))
+        champsList[champ["champion"]] = Champion(champ["champion"], champ["cost"], champ["traits"], champ["image"])
     return champsList
 
 def importTraits():
@@ -51,49 +54,76 @@ def initializeProgram():
 
 
 #Groups champions based on their common traits, prints based on printBool
-def groupByTraits(champ_list, traits_list, printBool):
+def groupByTraits(champDeck, printBool):
     groupedTraits = {}
-    for x in traits_list:
-        groupedTraits[x.name] = []
 
-    for champ in champ_list:
+    for champ in champDeck:
         name = champ.name
         traits = champ.traits
         for x in traits:
+            if x not in groupedTraits:
+                groupedTraits[x] = []
             groupedTraits[x].append(champ)
+
     if (printBool):
         for trait in groupedTraits:
             print(("{0:" + str(len(trait)) + "}[{1:1}]:").format(trait, len(groupedTraits[trait])))
             champs = groupedTraits[trait]
             for champ in champs:
-                print("\t" + str(champ))
+                print("\t" + champ.printName())
     return groupedTraits
 
+
 # Counts number of champs with specified trait, if number matches last entry in the array then the trait is gold tier and returned true
+
+
 def isTraitGold(deck, trait):
     counter = 0
     for champ in deck:
-        print(champ)
         if trait.name in champ.traits:
             counter+=1
+
     return counter >= trait.sets[len(trait.sets)-1]
 
+def getChamp(strName):
+    if strName in championsList:
+        return championsList[strName]
+    throwError(2, strName)
+    return Champion("ERROR",0,[],"")
 
+def getChampList(strList):
+    champions = []
+    for champ in strList:
+        champions.append(getChamp(champ))
+    return champions
 
+def getTrait(strName):
+    for trait in traitsList:
+        if trait.name == strName:
+            return trait
+    throwError(1, strName)
+    return Trait("ERROR","",[99])
 
+def throwError(errorCode, argument):
+    errorDict = {
+        1: "ERROR: TRAIT NOT FOUND: " + argument,
+        2: "ERROR: CHAMPION NOT FOUND: " + argument,
+        3: "ERROR: ITEM NOT FOUND: " + argument
+    }
+    print(errorDict.get(errorCode))
 
 
 ###########################
-def main(championsList, traitsList, itemsList):
-    testDeck = [championsList[14], championsList[29], championsList[34]]
-    print(isTraitGold(testDeck, traitsList[18]))
-    groupedChamps = groupByTraits(championsList, traitsList, 0)
+def main():
+    testDeck = getChampList(["KogMaw","RekSai","Skarner","Annie", "LeBlanc"])
+
+
+    print(getChamp("Veigar"))
 
 
 
-
-
+# INITIALIZE GLOBAL VARIABLES
+championsList , traitsList , itemsList = initializeProgram()
 
 # RUN FILE
-championsList , traitsList , itemsList = initializeProgram()
-main(championsList, traitsList, itemsList)
+main()
